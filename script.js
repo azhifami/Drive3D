a102cd12defa0b1580cf024af9528e531742332e
azhifami <azhifami@gmail.com>
2026-08-03 23:29:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,44 @@
+import * as THREE from "./build/three.module.js";
+import Renderer from "./engine/Renderer.js";
+
+const renderer = new Renderer();
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x87CEEB);
 
 const camera = new THREE.PerspectiveCamera(
-60,
-window.innerWidth/window.innerHeight,
-0.1,
-1000
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
 );
 
-const renderer = new THREE.WebGLRenderer({
-antialias:true
-});
+camera.position.set(0,5,8);
 
-renderer.setSize(window.innerWidth,window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const light = new THREE.HemisphereLight(
+    0xffffff,
+    0x666666,
+    2
+);
 
-document.getElementById("loading").remove();
-
-const light = new THREE.HemisphereLight(0xffffff,0x444444,2);
 scene.add(light);
 
 const ground = new THREE.Mesh(
-new THREE.PlaneGeometry(300,300),
-new THREE.MeshLambertMaterial({
-color:0x33aa33
-})
+    new THREE.PlaneGeometry(100,100),
+    new THREE.MeshStandardMaterial({
+        color:0x33aa33
+    })
 );
 
 ground.rotation.x = -Math.PI/2;
+
 scene.add(ground);
-
-const road = new THREE.Mesh(
-new THREE.BoxGeometry(8,0.05,300),
-new THREE.MeshLambertMaterial({
-color:0x444444
-})
-);
-
-road.position.y=0.03;
-scene.add(road);
-
-const car = new THREE.Mesh(
-new THREE.BoxGeometry(2,1,4),
-new THREE.MeshLambertMaterial({
-color:0xff0000
-})
-);
-
-car.position.y=0.6;
-scene.add(car);
-
-camera.position.set(0,5,10);
 
 function animate(){
 
-requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 
-camera.position.x=car.position.x;
-camera.position.z=car.position.z+10;
-camera.lookAt(car.position);
-
-renderer.render(scene,camera);
+    renderer.render(scene,camera);
 
 }
 
@@ -68,9 +46,6 @@ animate();
 
 window.addEventListener("resize",()=>{
 
-camera.aspect=window.innerWidth/window.innerHeight;
-camera.updateProjectionMatrix();
-
-renderer.setSize(window.innerWidth,window.innerHeight);
+    renderer.resize(camera);
 
 });

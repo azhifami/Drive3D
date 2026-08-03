3a2f7a874d7279d3e6f24e7911acddf4995cf23e
azhifami <azhifami@gmail.com>
2026-08-03 23:23:34 +0700

    Create Renderer.js

---
+++ engine/Renderer.js
@@ -0,0 +1,45 @@
+import * as THREE from "../build/three.module.js";
+
+export class Renderer {
+
+    constructor() {
+
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true
+        });
+
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+
+        this.renderer.setSize(
+            window.innerWidth,
+            window.innerHeight
+        );
+
+        this.renderer.shadowMap.enabled = true;
+
+        document.body.appendChild(
+            this.renderer.domElement
+        );
+
+    }
+
+    render(scene, camera) {
+        this.renderer.render(scene, camera);
+    }
+
+    resize(camera) {
+
+        camera.aspect =
+            window.innerWidth /
+            window.innerHeight;
+
+        camera.updateProjectionMatrix();
+
+        this.renderer.setSize(
+            window.innerWidth,
+            window.innerHeight
+        );
+
+    }
+
+}
